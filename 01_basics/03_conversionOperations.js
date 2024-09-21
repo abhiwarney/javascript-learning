cc57a669ee6b91547b7f0d9876d5ca42ceebdc9d
Abhinav <abhiwarney@gmail.com>
2024-09-21 12:42:56 +0000

    added diff operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -2,7 +2,7 @@ let aNumber = NaN //undefined|null
 
 let stringNUmber = String(aNumber) //converting to string
 
-console.table([stringNUmber,typeof stringNUmber]); 
+//console.table([stringNUmber,typeof stringNUmber]); 
 //if input = 33 , type string , value '33'
 //if input = undefined , type is string , value 'undefined'
 //input = true, type is string, value 'true'
@@ -31,3 +31,21 @@ let toBoolean = Boolean(aValue)
 //input  = null , ouput = false, type 'boolean'
 //input = undefined, output  = false, type 'boolean'
 //input = NaN, output  = false, type 'boolean'
+
+//----------------------------------------------------OPERATIONS---------------------------------------------------------
+console.log(1+1)
+console.log(2*2)
+console.log(4/2)
+console.log(3%2)
+console.log(2**3)
+let a  = 2
+console.log("a++",a++)
+console.log("++a :",++a)
+console.log(1+"3")
+console.log("1"+3)
+console.log("1"+"1")
+console.log(1+2+"3")//first add the numbers and then concat
+console.log("1"+2+2) //consider first string and automatically converts the result to string
+console.log(+true)//converts boolean to number
+console.log(true+4)
+console.log(true*4)
