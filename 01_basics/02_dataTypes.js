94e0795846f0db42a897e37ad5f8191b58430cc1
Abhinav <abhiwarney@gmail.com>
2024-09-21 13:40:24 +0000

    updated datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -5,6 +5,14 @@ let age = 16 //number
 let isSkilled = true //boolean
 let emp = {"name":"abhi","age":16,"skill" : "java"}
 let bigIntExample = BigInt(42535364757588563334647586856985738573858333783758937957736985353)//used for storing bigINt value > 2^53
+let bigIntSHortExample = 435464736374758574558585855n//'n' at the end automatically makes it a bigInt
+
+//symbol
+const id_01 = Symbol('123')
+const id_02 = Symbol('123')
+
+console.log("\nid_01 :",id_01,"id_02 :",id_02,"id_01 == id_02",id_01==id_02)
+
 console.log(emp)
 console.log(bigIntExample,"typeof :",typeof bigIntExample)
 //bigint => for big integer values > 2^53
