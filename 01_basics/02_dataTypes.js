21989b75858530c52f579fd20b098155a3702185
Abhinav <abhiwarney@gmail.com>
2024-09-21 09:56:13 +0000

    added datatypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict" //forces the engine to consider js to be of newer verison
+
+let name= "abhinav" //string
+let age = 16 //number
+let isSkilled = true //boolean
+
+
+
+
+//bigint => for big integer values > 2^53
+//undefined => a datatype
+//null => a standalone value
+//object  => also a datatype
+//symbol => for unique values (helpful in react for unique components)
+
+console.log(typeof name) // to know th etype of the variable 'typeof'
+
+console.log(typeof undefined) // output is 'undefined' type
+console.log(typeof null) // output is 'object' type 
