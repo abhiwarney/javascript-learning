fe42aa9a02f071c0f5a3e825444457062358a18d
Abhinav <abhiwarney@gmail.com>
2024-09-25 15:06:03 +0000

    updated object

--- 02_basics/04_Objects.js
+++ 02_basics/04_Objects.js
@@ -20,14 +20,14 @@ const obj3 = {3 : 'c' , 4 : 'd'}
 
 //to combine two objects
 const obj4 = {obj2,obj3}
-console.log(obj4) 
+//console.log(obj4) 
 /* out put is not expected of above code:
     { obj2: { '1': 'a', '2': 'b' }, obj3: { '3': 'c', '4': 'd' } }
 */
 //using assign() function of object 
 
 const obj5 = Object.assign({},obj2,obj3)
-console.log(obj5); //output : -> { '1': 'a', '2': 'b', '3': 'c', '4': 'd' } (this is expected outptut)
+//console.log(obj5); //output : -> { '1': 'a', '2': 'b', '3': 'c', '4': 'd' } (this is expected outptut)
 /*note
 in above code 'Object.assign({},obj2,obj3)' here '{}' is specified as target object where the accumulation is done and returned, if
 we donot provide then still the new object will have the same output but the accumulation will happen at obj2 object and will reflect in obj2,
@@ -47,7 +47,18 @@ and
 obj3 :  { '3': 'c', '4': 'd' }
 ---------------------------------------------------
 */
-console.log("before operation \nobj2 : ",obj2,"\nand \nobj3 : ",obj3);
-const obj6 = Object.assign(obj2,obj3)
-console.log("result object obj6 : ", obj6)
-console.log("after assign operation \nobj2 : ",obj2,"\nand \nobj3 : ",obj3);
+// console.log("before operation \nobj2 : ",obj2,"\nand \nobj3 : ",obj3);
+ const obj6 = Object.assign(obj2,obj3)
+// console.log("result object obj6 : ", obj6)
+// console.log("after assign operation \nobj2 : ",obj2,"\nand \nobj3 : ",obj3);
+
+// The above methods are rarely used for combining objects, instead we used spread() to combines 90% of the time
+const obj7 = {...obj2,...obj3} //this simply returns new object
+//console.log(obj7)
+
+//varipuse function available
+console.log(Object.keys(obj2))// results in array of all the keys inside obj  | output: [ '1', '2' ]
+console.log(Object.values(obj2)) // results in array of values | outptut : [ 'a', 'b' ]
+console.log(Object.entries(obj2)) // results in key value pair inside array of array | output : [ [ '1', 'a' ], [ '2', 'b' ] ]
+console.log(obj1.hasOwnProperty("idf")) //check if property is present in the object
+console.log(obj1.name?.fullName.firstName) //?. (optional chaining) is used if prpeerty is undefined or null then return undefined instead of throwing error
