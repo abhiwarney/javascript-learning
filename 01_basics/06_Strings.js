f8a0499d030fd83b17a456e7f7be871379ce63b3
Abhinav <abhiwarney@gmail.com>
2024-09-25 08:53:24 +0000

    added String.js

---
+++ 01_basics/06_Strings.js
@@ -0,0 +1,18 @@
+let name0 = "   Abhinav  "
+let age = 26
+let neString = new String("Hello-Wolrd-Welcome-TO-India")
+console.log(`Hello ${name0} , your age is ${age}`); //This is called String interpolation, where we inject the variables through placeholders and string is enclose in "` `" back ticks
+
+console.log(neString);
+console.log(neString.__proto__);
+console.log(name0.toUpperCase());
+console.log(neString.split('-'))
+console.log(name0.trim());
+console.log(neString.substring(11,8))
+console.log(neString.charAt(0))
+console.log(neString);
+
+console.log("slice: ",neString.slice(-10,-1))
+
+//to-do explore String functiomon mdn website
+
