d42a71076396147265339c79936641bd7e5eba92
Abhinav <abhiwarney@gmail.com>
2024-09-27 10:25:37 +0000

    updated arrows.js

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -11,9 +11,78 @@ const anyObject = {
     greetings : function(){ console.log(`${this.name} welcome to Arrows concept`)} // her 'this' is refering to current object
 }
 
-anyObject.greetings() // output - > abhi welcome to Arrows concept
+//anyObject.greetings() // output - > abhi welcome to Arrows concept
 
 //but lets try this with global context, what shouold be th output in case of node
 
-console.log(this) //this return {} an empty object, but if do the same thing in browser it will return the window global object, and this the major diff
+//console.log(this) //this return {} an empty object, but if do the same thing in browser it will return the window global object, and this the major diff
 
+
+//lets try this in funcitons
+
+function aFunc(){
+    const a = "hello"
+    //console.log(this) // this will return below long code *exmaple = 01
+    //console.log(this.a) //this will return 'undefined'
+}
+
+aFunc() 
+/*
+(exmaple =01)
+---------------------------
+<ref *1> Object [global] {
+  global: [Circular *1],
+  clearImmediate: [Function: clearImmediate],
+  setImmediate: [Function: setImmediate] {
+    [Symbol(nodejs.util.promisify.custom)]: [Getter]
+  },
+  clearInterval: [Function: clearInterval],
+  clearTimeout: [Function: clearTimeout],
+  setInterval: [Function: setInterval],
+  setTimeout: [Function: setTimeout] {
+    [Symbol(nodejs.util.promisify.custom)]: [Getter]
+  },
+  queueMicrotask: [Function: queueMicrotask],
+  structuredClone: [Function: structuredClone],
+  atob: [Getter/Setter],
+  btoa: [Getter/Setter],
+  performance: [Getter/Setter],
+  fetch: [Function: fetch],
+  crypto: [Getter]
+}
+*/
+
+//lets try with another style of funciton declaartion
+
+const myFunct = function(){
+    const a = "hello "
+    //console.log(this) //this will also return output *example =01
+    //console.log(this.a) //return undefined
+}
+
+myFunct()
+
+
+
+//lets talk about arrow function, its nothing but lambda expression in js, 
+
+const aFunction = () => {
+    const a = "hello"
+    //console.log(this) //in arrows style , this will only return an empty object, output -> {}
+    //console.log(this.a) //ouptu 'undefined'
+} 
+aFunction()
+
+//so basically this function return an empty object in case of arrow funcitons
+
+//now lets try to explore arrow function in js, this almost similar to lambda expression in java
+
+const anArrowFunciton = (a,b) => a + b // return stament is not needed when there is only one line, but if yoy specify {} then return stament becomes mandatory
+console.log(anArrowFunciton(3,4))
+
+//now lets try to return an object using arrow
+
+const returObjectFunc = () => ({name: "abhinav"}) //remember this syntax , as this will be helpul in react 
+console.log(returObjectFunc()) //ouput undefined
+
+// the problem here is {name : "abhinav"} should be enclose in a parenthesis '()' or enclose them { return { name : "abhinav"}} in this way
